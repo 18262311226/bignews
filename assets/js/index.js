2fded1286de9661e7d0479cbc8e38bf6885807ef
err-pied piper <3435516833@qq.com>
2020-09-23 21:35:04 +0800

    开始个人中心搭建

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,19 +1,31 @@
 $.ajax({
     type:"get",
     url:"/my/userinfo",
-    headers:{
-        Authorization:window.localStorage.getItem("token")
-    },
     success:function(res){
-        $(".userinfo .welcome").html(`欢迎&nbsp;&nbsp;${res.data.username}`);
-
-        if(res.data.user_pic){
-            $(".userinfo .layui-nav-img").show().attr("src",res.data.user_pic);
-            $(".layui-tx .layui-nav-img").show().attr("src",res.data.user_pic);
-            $(".userinfo .text-avater,.layui-tx .text-avater").hide();
-        }else{
-            $(".userinfo .text-avater").text(res.data.username.slice(0,1).toUpperCase());
-            $(".layui-tx .text-avater").text(res.data.username.slice(0,1).toUpperCase());
+        if(res.status === 0){
+            $(".userinfo .welcome").html(`欢迎&nbsp;&nbsp;${res.data.username}`);
+            if(res.data.user_pic){
+                $(".userinfo .layui-nav-img").show().attr("src",res.data.user_pic);
+                $(".layui-tx .layui-nav-img").show().attr("src",res.data.user_pic);
+                $(".userinfo .text-avater,.layui-tx .text-avater").hide();
+            }else{
+                $(".userinfo .text-avater").text(res.data.username.slice(0,1).toUpperCase());
+                $(".layui-tx .text-avater").text(res.data.username.slice(0,1).toUpperCase());
+            }
         }
     }
 });
+
+$(".loginout").click(function(){
+    layer.confirm('确定退出?', {icon: 3, title:'提示'}, function(index){
+        //do something 写一些业务逻辑
+         // 2.3 删除本地存储中的token
+        window.localStorage.removeItem('token');
+  
+        // 2.4 关闭弹出层
+        layer.close(index);
+  
+        // 2.5 跳转到login.html页面
+        location.href = './login.html';
+    });
+})
--- assets/js/utils.js
+++ assets/js/utils.js
@@ -1,10 +1,19 @@
 $.ajaxPrefilter(function(options){
-    // 这个函数是在所有的ajax请求之前执行的
-    // options是一个对象，里面存储了所有的ajax函数中的参数内容
-    // console.log(456);
-    // console.log(options);
-    // 就可以将所有的请求要路径进行统一的设置
-    options.url = 'http://ajax.frontend.itheima.net' + options.url
+    options.url = 'http://ajax.frontend.itheima.net' + options.url;
   
-    
+    if(options.url.includes("/my")){
+        options.headers  = {
+          Authorization:window.localStorage.getItem("token")
+        }  
+    }
+
+    options.complete = function(res){
+      
+      if(res.responseJSON.status == 1 && res.responseJSON.message == "身份认证失败！"){
+        // 删除本地中的无效token
+        console.log(1111);
+        // 应该先跳转到登陆页面进行登陆
+        location.href = './login.html';
+      }
+    }
   })
